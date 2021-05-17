08ff293d870f5a6af87764cee81078bf7f73d1a8
Jhoan Porcel Peña <porcel.pena.jhoan@gmail.com>
2021-05-17 15:32:39 -0400

    subiendo primera version de waliki

---
+++ src/main/java/com/example/Waliki/WalikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Waliki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Waliki/WalikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Waliki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
